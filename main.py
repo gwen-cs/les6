4e535a851fcc6abb30e6225c09a377e35a54f3d7
Gwen Reynders <gwenneke@pm.me>
2026-07-10 18:54:11 +0200

    flash message

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, flash
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
 app = Flask(__name__)
+app.secret_key = os.getenv("APP_SECRET_KEY")
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/success")
+def success():
+    flash("Operation completed successfully")
+    return redirect(url_for("index"))
 
